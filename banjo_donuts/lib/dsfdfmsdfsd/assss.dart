0d6c8ef0f1d3c075b39c3ffc7de23358a8cd6a4f
MihailDundukov <misha2001du@gmail.com>
2024-05-07 21:25:24 +0300

    1

---
+++ banjo_donuts/lib/dsfdfmsdfsd/assss.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class KletkaModel1111 {
+  late final Offset cenPositiondDon;
+
+  late int xKDon;
+  late int yKDon;
+
+  KletkaModel1111();
+
+  void generateP11o1211qqq212121221sKlD(bool aaaaaaas, int yDon) {
+    xKDon = aaaaaaas == false ? 1 : 0;
+    yKDon = yDon;
+    cenPositiondDon = Offset((xKDon).toDouble(), (250 + yKDon).toDouble());
+    final premS = false;
+  }
+}
